33d33e3a20675ba2e55d94ecbe47e3d8be30159a
Sanni Faisal <42116495+olaoluwa123@users.noreply.github.com>
2025-10-17 17:46:53 +0100

    Update urls.py

--- pos_app/urls.py
+++ pos_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path('nextpage.html', views.next_page, name='next_page'),
+    path('page2', views.next_page, name='next_page'),
 ]
