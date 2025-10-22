e713dbc88499b8f09204d960b3989a21dea2eb7e
Sanni Faisal <42116495+olaoluwa123@users.noreply.github.com>
2025-10-22 14:56:09 +0100

    Update settings.py

--- pos_specs/settings.py
+++ pos_specs/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+PORT = os.environ.get('PORT', '8000')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'template')
