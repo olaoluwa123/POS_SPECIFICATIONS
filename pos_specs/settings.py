f4a3915e63b728f6b0827be0b2ae486a05a8b1d5
Sanni Faisal <42116495+olaoluwa123@users.noreply.github.com>
2025-10-22 11:31:53 +0100

    Update settings.py

--- pos_specs/settings.py
+++ pos_specs/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=bojc#5t#+p=xrujx*oda__wck0s_4os7o6kwp*f3)p@)pm7z4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pos-specifications.onrender.com']
 
 
 
